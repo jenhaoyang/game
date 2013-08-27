c82fdaf0fb8d9c0623b386c635f2a1936ae7cb23
mrobinson7627 <mrobinson7627@gmail.com>
2013-08-26 23:35:30 -0400

    More initial work

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,11 @@
 #include <SFML/System.hpp>
 #include "State.h"
 
+StateChanger statechanger;
+
 int main() {
     sf::RenderWindow window(sf::VideoMode(800, 600), "Game");
     window.setMouseCursorVisible(false);
-    StateChanger statechanger;
     statechanger.changeState(STATE_TEST);
     while (window.isOpen()) {
         statechanger.current_state->logic(window);
