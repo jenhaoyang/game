dce0b857de968ab4af1ad59fe7bd40e9a1a04a54
Matt Robinson <mrobinson7627@gmail.com>
2014-05-18 22:48:45 -0400

    Added some globals

---
+++ include/Globals.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#define RADTODEG (b2_pi / 180.0)
+#define DEGTORAD (1.0 / RADTODEG)
+
+//Pixels per Meter
+//Character is 6 feet (~1.82 meters) tall and about 22 pixels tall
+#define PM 40.0
+#define UPM (1.0 / RATIO)
